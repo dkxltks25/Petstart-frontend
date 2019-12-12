66bff1360c9d194aefda9a220610e0beda23f9c5
dkxltks25 <dkxltks25@naver.com>
2019-12-12 17:47:57 +0900

    testUpload

--- src/Component/LineChart.js
+++ src/Component/LineChart.js
@@ -1,48 +1,220 @@
 import React from 'react';
 import {Line} from 'react-chartjs-2';
-import { myPetIs } from '../Routes/Register/RegisterQuery';
 import { useQuery } from 'react-apollo-hooks';
 import { SELECTTEMP } from '../Routes/Main/MainQueries';
-const Data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40,65, 59, 80, 81, 56, 55, 40]
+import _ from "loadsh";
+import Loader from './Loader';
+
+const ToMonth = (State,data,FirstData,LastData)=>{
+  const FirstDate = new Date(FirstData).getMonth()+1;
+  const LastDate = new Date(LastData);
+  const LastDay = new Date(2019,FirstDate,0).getDate();
+  const {selectTemp} = data;  
+  const Test = selectTemp.filter((index)=>{
+    const Month = new Date(index["createdAt"]).getMonth()+1;
+    return Month === FirstDate;
+  });
+  const Test1 =_.groupBy(Test,(a)=> new Date(a["createdAt"]).getDate());
+  console.log(Test1);
+  let NowKey = 1;
+  const DateTimeArray = [];
+
+  for(let i = 1; i<=LastDay;i++){
+    if(Test1[i] === undefined){
+      DateTimeArray.push([0,i]);
+    }else{
+      let Count = 0;
+      let Sum = 0;
+      Test1[i].map(({Temp})=>{
+        Count++;
+        Sum+=Temp;
+      });
+      DateTimeArray.push([parseFloat((Sum/Count).toFixed(3)),i]);
     }
-  ] 
-};
-
-
-export default ({deviceName})=>{
-  const {data,loading} = useQuery(SELECTTEMP,{variables:{deviceName}});
-  if(!loading){
-    console.log(data);
   }
+  const label = [];
+  const date = [];
+   DateTimeArray.map(index=>{
+      label.push(index[1]+"일");
+      date.push(index[0]);
+  })
+  const GraphData = {
+    labels: label,
+    datasets: [
+      {
+        label: State,
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: date
+      }
+    ] 
+  };
+  return GraphData;
+}
+const toWeek = (State,data,FirstData,LastData)=>{
+    const FirstDate = new Date(FirstData).getDay();
+    const LastDate = new Date(LastData).getDay();
+    const LastDay = new Date(2019,FirstDate,0).getDate();
+    const {selectTemp} = data;  
+    const Test = selectTemp.filter((index)=>{
+      const Month = new Date(index["createdAt"]).getMonth()+1;
+      return Month === FirstDate;
+    });
+    const Test1 =_.groupBy(Test,(a)=> new Date(a["createdAt"]).getDate());
+    let NowKey = 1;
+    const DateTimeArray = [];
+  
+    for(let i = FirstDate; i<=LastDate;i++){
+      if(Test1[i] === undefined){
+        DateTimeArray.push([0,i]);
+      }else{
+        let Count = 0;
+        let Sum = 0;
+        Test1[i].map(({Temp})=>{
+          Count++;
+          Sum+=Temp;
+        });
+        DateTimeArray.push([parseFloat((Sum/Count).toFixed(3)),i]);
+      }
+    }
+    const label = [];
+    const date = [];
+     DateTimeArray.map(index=>{
+        label.push(index[1]+"일");
+        date.push(index[0]);
+    })
+    const GraphData = {
+      labels: label,
+      datasets: [
+        {
+          label: State,
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: date
+        }
+      ] 
+    };
+    return GraphData;
+  
+}
+const toDay = (State,data,FirstData)=>{
+    const FirstDate = new Date(FirstData).getMonth()+1;
+    const NowDay = new Date(FirstData).getDate();
+    const {selectTemp} = data;  
+    const Test = selectTemp.filter((index)=>{
+      const Month = new Date(index["createdAt"]).getMonth()+1;
+      const Day = new Date(index["createdAt"]).getDate();
+      console.log(Month,Day);
+      return Month === FirstDate && Day === NowDay;
+    });
+    console.log(Test);
+    const Test1 =_.groupBy(Test,(a)=> new Date(a["createdAt"]).getHours());
+    let NowKey = 1;
+    const DateTimeArray = [];
+  
+    for(let i = 1; i<=24;i++){
+      if(Test1[i] === undefined){
+        DateTimeArray.push([0,i]);
+      }else{
+        let Count = 0;
+        let Sum = 0;
+        Test1[i].map(({Temp})=>{
+          Count++;
+          Sum+=Temp;
+        });
+        DateTimeArray.push([parseFloat((Sum/Count).toFixed(3)),i]);
+      }
+    }
+    const label = [];
+    const date = [];
+     DateTimeArray.map(index=>{
+        label.push(index[1]+"시");
+        date.push(index[0]);
+    })
+    const GraphData = {
+      labels: label,
+      datasets: [
+        {
+          label: State,
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: date
+        }
+      ] 
+    };
+    console.dir(GraphData);
+    return GraphData;
+
+}
+const SetGraph = (State,data,FirstData,LastData)=>{
+  if(State === "이번달 통계"){
+    return ToMonth(State,data,FirstData,LastData);
+  }
+  if(State === "이번주 통계"){
+    return toWeek(State,data,FirstData,LastData);
+  }else{
+    console.dir(toDay(State,data,FirstData));
+    return toDay(State,data,FirstData)
+  }
+
+  
+}
+
+export default ({State,deviceName,FirstData,LastData})=>{
+  const Status = State === "toDay" ? "오늘의 통계": State === "toWeek" ? "이번주 통계": "이번달 통계";
+  const {data,loading} = useQuery(SELECTTEMP,{variables:{deviceName}});
+  
+ 
+
   return(
         <div>
-        <h2>이번주 체온</h2>
-        <Line data={Data} />
-        <Line data={Data} />
-        <Line data={Data} />
-        <Line data={Data} />
+        <h2>{Status}</h2>
+        {!loading ? (<Line data={SetGraph(Status,data,FirstData,LastData)} />) : (<Loader></Loader>)}
       </div>
     )
 }
--- src/Routes/Main/MainPresent.js
+++ src/Routes/Main/MainPresent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'date-fns';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from "@material-ui/core/Paper";
 import { Typography, Link } from '@material-ui/core';
@@ -6,7 +7,51 @@ import Line from "../../Component/LineChart";
 import { useQuery } from "react-apollo-hooks";
 import { myPet } from "../Register/RegisterQuery";
 import Loader from "../../Component/Loader.js";
+import Grid from '@material-ui/core/Grid';
+import {Tabs,Tab,Box} from "@material-ui/core";
+
+
+import Speech from 'react-speech';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+    KeyboardTimePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-auto-tabpanel-${index}`}
+        aria-labelledby={`scrollable-auto-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box p={3}>{children}</Box>}
+      </Typography>
+    );
+  }
+
 export default ()=>{
+    const [selectedFirstDate, setSelectedFirstDate] = React.useState(new Date());
+    const [selectedLastDate, setSelectedLastDate] = React.useState(new Date());
+
+    const handleFirstDateChange = date => {
+        setSelectedFirstDate(date);
+    };
+    const handleLastDateChange = date => {
+        setSelectedLastDate(date);
+    };
+    const [Tabvalue,TabsetValue] = React.useState();
+    const TabhandleChange = (event,newValue)=>{
+        TabsetValue(newValue);
+    }
+    
+
     const useStyle = makeStyles(theme=>({
         Container:{
             width:1000,
@@ -66,14 +111,43 @@ export default ()=>{
             {loading === true ? <Loader/> : ( 
             <>
             <Paper className={Classes.leftContainer}>
+            <Tabs
+        value={Tabvalue}
+        onChange={TabhandleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="오늘의 통계" />
+        <Tab label="이번주 통계" />
+        <Tab label="이번달 통계" />
+      </Tabs>
+      <TabPanel value={Tabvalue} index={0}>
             {data &&
             data.myPets &&
             data.myPets.length === 1 ? (
-                <div className={Classes.AnimalInfo}>
 
+            <div className={Classes.AnimalInfo}>
                 <div className = {Classes.AnimalInfoBottom}>
-                <Line deviceName ={data.myPets[0].deviceName} className = {Classes.HealthChart}></Line>
+                    <Line State = "toDay" deviceName ={data.myPets[0].deviceName} className = {Classes.HealthChart} FirstData={selectedFirstDate} LastData={selectedLastDate}>
+                    </Line>
                 </div>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+            <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date picker dialog"
+                format="MM/dd/yyyy"
+                value={selectedFirstDate}
+                onChange={handleFirstDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+                
+            </Grid>
+            </MuiPickersUtilsProvider>
             </div>
             ):(
                 <div className ={Classes.LinkWrap}>
@@ -83,13 +157,109 @@ export default ()=>{
                         </Link>
                     </Typography>
                 </div>
-            )}
+            )}    
+            </TabPanel>
+            <TabPanel value={Tabvalue} index={1}>
+            {data &&
+            data.myPets &&
+            data.myPets.length === 1 ? (
 
-                
+            <div className={Classes.AnimalInfo}>
+                <div className = {Classes.AnimalInfoBottom}>
+                    <Line State = "toWeek" deviceName ={data.myPets[0].deviceName} className = {Classes.HealthChart} FirstData={selectedFirstDate} LastData={selectedLastDate}>
+                    </Line>
+                </div>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+            <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date picker dialog"
+                format="MM/dd/yyyy"
+                value={selectedFirstDate}
+                onChange={handleFirstDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+                <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date picker dialog"
+                format="MM/dd/yyyy"
+                value={selectedLastDate}
+                onChange={handleLastDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+            </Grid>
+            </MuiPickersUtilsProvider>
+            </div>
+            ):(
+                <div className ={Classes.LinkWrap}>
+                    <Typography>
+                        <Link href="#/Register">
+                            정보를 등록하러 가시겠습니까?
+                        </Link>
+                    </Typography>
+                </div>
+            )}    
+            </TabPanel>    
+            <TabPanel value={Tabvalue} index={2}>
+            {data &&
+            data.myPets &&
+            data.myPets.length === 1 ? (
+
+            <div className={Classes.AnimalInfo}>
+                <div className = {Classes.AnimalInfoBottom}>
+                    <Line State = "toMonth" deviceName ={data.myPets[0].deviceName} className = {Classes.HealthChart} FirstData={selectedFirstDate} LastData={selectedLastDate}>
+                    </Line>
+                </div>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+            <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date picker dialog"
+                format="MM/dd/yyyy"
+                value={selectedFirstDate}
+                onChange={handleFirstDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+                <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                label="Date picker dialog"
+                format="MM/dd/yyyy"
+                value={selectedLastDate}
+                onChange={handleLastDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
+            </Grid>
+            </MuiPickersUtilsProvider>
+            </div>
+            ):(
+                <div className ={Classes.LinkWrap}>
+                    <Typography>
+                        <Link href="#/Register">
+                            정보를 등록하러 가시겠습니까?
+                        </Link>
+                    </Typography>
+                </div>
+            )}    
+            </TabPanel>                    
             </Paper>
+            
             <Paper className={Classes.rightContainer}>
                 <div className={Classes.ParseInfo}>
-                  <Paper></Paper>
+                  <Paper>
+                   
+                  </Paper>
                 </div>
             </Paper>
             </>
